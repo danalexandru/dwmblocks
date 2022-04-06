4bf1babd5f2a33d65a86a07ecd2941ca84e25c5f
danalexandru <danb.alexandru@gmail.com>
2022-04-06 21:24:03 +0300

    add: Added indicator of internet connection (icon only for WIFI)

--- blocks.h
+++ blocks.h
@@ -2,9 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-    {" ",     " . /home/aldan/.config/scripts/get_keyboard_layout.sh",    1,      0},
-    {"",       " . /home/aldan/.config/scripts/get_battery_percentage.sh", 300,    0},
-    {"",       " . /home/aldan/.config/scripts/get_date_time.sh",          1,    0},
+    {" ",     " . /home/aldan/.config/scripts/get_keyboard_layout.sh",         1,      0},
+    {"",       " . /home/aldan/.config/scripts/check_internet_connection.sh",   10,     0},
+    {"",       " . /home/aldan/.config/scripts/get_battery_percentage.sh",      300,    0},
+    {"",       " . /home/aldan/.config/scripts/get_date_time.sh",               1,      0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
