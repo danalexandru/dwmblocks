18fea6812408280a148da98aa34a423beee61052
danalexandru <danb.alexandru@gmail.com>
2022-04-06 20:39:43 +0300

    update: Updated the date-time blocks

--- blocks.h
+++ blocks.h
@@ -4,8 +4,7 @@ static const Block blocks[] = {
 	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
     {" ",     " . /home/aldan/.config/scripts/get_keyboard_layout.sh",    1,      0},
     {"",       " . /home/aldan/.config/scripts/get_battery_percentage.sh", 300,    0},
-	{" ",     " date '+%a %d %b %Y'",					                  36000,  0},
-    {" ",     " date '+%H:%M:%S'",                                       1,      0},
+    {"",       " . /home/aldan/.config/scripts/get_date_time.sh",          1,    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
