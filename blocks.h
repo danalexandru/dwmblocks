8e8bf35a440a3d28b4e08568ef076adb7423cd9d
danalexandru <danb.alexandru@gmail.com>
2022-04-06 21:30:42 +0300

    add: Added bluetooth icon

--- blocks.h
+++ blocks.h
@@ -4,6 +4,7 @@ static const Block blocks[] = {
 	//{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
     {" ",     " . /home/aldan/.config/scripts/get_keyboard_layout.sh",         1,      0},
     {"",       " . /home/aldan/.config/scripts/check_internet_connection.sh",   10,     0},
+    {"",       " . /home/aldan/.config/scripts/check_if_bluetooth.sh",          5,      0},
     {"",       " . /home/aldan/.config/scripts/get_battery_percentage.sh",      300,    0},
     {"",       " . /home/aldan/.config/scripts/get_date_time.sh",               1,      0},
 };
